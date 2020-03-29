5185c95d8533e83a39e44a709108bd935f5acd12
maxim <admtrigger@gmail.com>
2020-03-29 19:11:01 +0300

    vs code

--- Udemy/react/react_tutorial_projects_cource/section8_to-do-list/src/App.js
+++ Udemy/react/react_tutorial_projects_cource/section8_to-do-list/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
+// showwing vs-code
 function App() {
   return (
     <>
