2bded023b2d39a0c4b00b49e0221584ca93980d6
maxim <admtrigger@gmail.com>
2020-05-03 16:41:58 +0300

    monster rolodex
    monster rolodex

--- Udemy/react/complete-react-developer-zero-to-mastery/section3-react-basics/monsters-rolodex/src/App.js
+++ Udemy/react/complete-react-developer-zero-to-mastery/section3-react-basics/monsters-rolodex/src/App.js
@@ -1,23 +1,31 @@
 import React, {Component} from 'react';
+import CardList from './components/card-list/CardList';
+import SearchBox from './components/searchBox/SearchBox';
 import './App.css';
 
 class App extends Component {
   state = {
-    monsters: []
+    monsters: [],
+    serchFiels: ''
   }
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(data => this.setState({monsters: data}))
   };
+
+  handleChange = (e) => {
+    this.setState({serchFiels: e.target.value})
+  }
+
   render() {
+    const {monsters,serchFiels} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(serchFiels.toLocaleLowerCase()))
     return (
       <div className="App">
-      {
-        this.state.monsters.map(item => {
-          return <h1 key={item.id}>{item.name}</h1>
-        })
-      }
+        <h1>monster rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />      
     </div>
     )
   }
